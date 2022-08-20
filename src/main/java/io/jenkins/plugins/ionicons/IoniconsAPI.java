2ef0d303361381b05e77a0320fde7b20f7a391da
Alexander Brandes <mc.cache@web.de>
2022-08-20 18:47:28 +0200

    feat: Initial work

---
+++ src/main/java/io/jenkins/plugins/ionicons/IoniconsAPI.java
@@ -0,0 +1,9 @@
+package io.jenkins.plugins.ionicons;
+
+/**
+ * Dummy class to make Jenkins HPI plug-in and InjectedTests happy.
+ */
+@SuppressWarnings("unused")
+public class IoniconsAPI {
+    // empty
+}
