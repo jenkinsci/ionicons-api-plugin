d338a3a98b48832d4a244e0e0b4c82ca48d84df8
NotMyFault <mc.cache@web.de>
2022-08-21 01:33:09 +0200

    chore: Drop obsolete dummy classes

--- src/main/java/io/jenkins/plugins/ionicons/IoniconsAPI.java
+++
@@ -1,9 +0,0 @@
-package io.jenkins.plugins.ionicons;
-
-/**
- * Dummy class to make Jenkins HPI plug-in and InjectedTests happy.
- */
-@SuppressWarnings("unused")
-public class IoniconsAPI {
-    // empty
-}
