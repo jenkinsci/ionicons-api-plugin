af3a84439e5ff7fcb37de6808c8a658e27f5e980
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2022-09-25 13:40:18 +0200

    Fix resource resolution of icons in jar file and IDE (#18)

--- src/test/java/io/jenkins/plugins/ionicons/IoniconsTest.java
+++ src/test/java/io/jenkins/plugins/ionicons/IoniconsTest.java
@@ -1,9 +1,8 @@
 package io.jenkins.plugins.ionicons;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.util.Collection;
@@ -13,41 +12,38 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test Class for {@link Ionicons}.
- * 
- * @author strangelookingnerd
  *
+ * @author strangelookingnerd
  */
 class IoniconsTest {
 
     @Test
     void testGetIconClassName() {
-	assertEquals("symbol-test plugin-ionicons-api", Ionicons.getIconClassName("test"));
+        assertEquals("symbol-test plugin-ionicons-api", Ionicons.getIconClassName("test"));
     }
 
     @Test
     void testGetAvailableIcons() {
-	Map<String, String> availableIcons = Ionicons.getAvailableIcons();
+        Map<String, String> availableIcons = Ionicons.getAvailableIcons();
 
-	assertNotNull(availableIcons);
-	assertFalse(availableIcons.isEmpty());
+        assertNotNull(availableIcons);
+        assertFalse(availableIcons.isEmpty());
 
-	Collection<File> icons = FileUtils.listFiles(new File("./src/main/resources/images/symbols/"),
-		new String[] { "svg" }, false);
-	assertEquals(icons.size(), availableIcons.size());
+        Collection<File> icons = FileUtils.listFiles(new File("./src/main/resources/images/symbols/"),
+                new String[]{"svg"}, false);
+        assertEquals(icons.size(), availableIcons.size());
 
-	List<String> iconNames = icons.stream().map(icon -> StringUtils.removeEnd(icon.getName(), ".svg"))
-		.sorted(Comparator.naturalOrder()).collect(Collectors.toList());
-	assertIterableEquals(iconNames, availableIcons.keySet());
+        List<String> iconNames = icons.stream().map(icon -> StringUtils.removeEnd(icon.getName(), ".svg"))
+                .sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+        assertIterableEquals(iconNames, availableIcons.keySet());
 
-	for (Entry<String, String> icon : availableIcons.entrySet()) {
-	    assertEquals(icon.getValue(), Ionicons.getIconClassName(icon.getKey()));
-	}
+        for (Entry<String, String> icon : availableIcons.entrySet()) {
+            assertEquals(icon.getValue(), Ionicons.getIconClassName(icon.getKey()));
+        }
     }
 
 }
