6d00ae68818d64c9314ec7762b423166e5d1c84e
NotMyFault <mc.cache@web.de>
2025-07-30 19:02:23 +0200

    Move from commons-lang 2 to 3

--- src/test/java/io/jenkins/plugins/ionicons/IoniconsTest.java
+++ src/test/java/io/jenkins/plugins/ionicons/IoniconsTest.java
@@ -1,7 +1,7 @@
 package io.jenkins.plugins.ionicons;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
